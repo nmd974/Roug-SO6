26b9c1527e86c8704703a5102150f931d15df2d7
nmd974 <jha.payet@gmail.com>
2020-10-26 09:35:43 +0400

    Chiffres cles + qui sommes nous

---
+++ js/chiffre_anime.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() { // Tells the function to wait to preform until everything on the page has loaded.
+    $(window).scroll(function() { // Says this function is preformed continuisly while scrolling.
+        var Scroll = $(window).scrollTop() + 1, // This variable finds the distance you have scrolled from the top.
+                    SectionOneOffset = $('#chiffres').offset().top, // This variable finds the distance between #section-one and the top. Replace #section-one with the ID of your section.
+                    SectionTwoOffset = $('#section-two').offset().top; // This variable finds the distance between #section-two and the top. Replace #section-two with the ID of your section. You can duplicate this for as many sections as you want.
+
+        if (Scroll >= SectionOneOffset) { // If you have scrolled past section one do this.
+            $('#trajets').val(() => {
+                setInterval(addNumber(1), 200)
+            })
+            $(".menu-item-1").addClass("current-menu-item"); // Adds class of current-menu-item to the menu item with a class of menu-item-1
+        } else { // If you have not scrolled section one do this.
+            $(".menu-item-1").removeClass("current-menu-item"); // Removes class of current-menu-item to the menu item with a class of menu-item-1
+        }
+            if (Scroll >= SectionTwoOffset) { // If you have scrolled past section two do this.You can duplicate this for as many sections as you want.
+            $(".menu-item-2").addClass("current-menu-item"); // Adds class of current-menu-item to the menu item with a class of menu-item-2
+                    $(".menu-item-1").removeClass("current-menu-item"); // Removes class of current-menu-item to the menu item with a class of menu-item-1
+        } else { // If you have not scrolled section two do this.
+            $(".menu-item-2").removeClass("current-menu-item"); // Removes class of current-menu-item to the menu item with a class of menu-item-2
+        }
+    });
+    function addNumberTrajet(indice) {
+        
+    }
+});
