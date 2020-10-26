b44f9ed79c667b84e86d6c0810a132d7146a8b14
nmd974 <jha.payet@gmail.com>
2020-10-26 11:50:42 +0400

    jquery

--- js/chiffre_anime.js
+++ js/chiffre_anime.js
@@ -2,13 +2,11 @@ $( document ).ready(function() { // Tells the function to wait to preform until 
     $(window).scroll(function() { // Says this function is preformed continuisly while scrolling.
         var Scroll = $(window).scrollTop() + 1, // This variable finds the distance you have scrolled from the top.
                     SectionOneOffset = $('#chiffres').offset().top, // This variable finds the distance between #section-one and the top. Replace #section-one with the ID of your section.
-                    SectionTwoOffset = $('#section-two').offset().top; // This variable finds the distance between #section-two and the top. Replace #section-two with the ID of your section. You can duplicate this for as many sections as you want.
+                    SectionTwoOffset = $('#confiance').offset().top; // This variable finds the distance between #section-two and the top. Replace #section-two with the ID of your section. You can duplicate this for as many sections as you want.
 
         if (Scroll >= SectionOneOffset) { // If you have scrolled past section one do this.
             console.log("COUCOU");
-            $('#trajets').val(() => {
-                setInterval(addNumber(1), 200)
-            })
+            
             $(".menu-item-1").addClass("current-menu-item"); // Adds class of current-menu-item to the menu item with a class of menu-item-1
         } else { // If you have not scrolled section one do this.
             $(".menu-item-1").removeClass("current-menu-item"); // Removes class of current-menu-item to the menu item with a class of menu-item-1
@@ -20,7 +18,4 @@ $( document ).ready(function() { // Tells the function to wait to preform until 
             $(".menu-item-2").removeClass("current-menu-item"); // Removes class of current-menu-item to the menu item with a class of menu-item-2
         }
     });
-    function addNumberTrajet(indice) {
-        
-    }
 });
