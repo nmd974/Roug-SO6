a924d3b374473080b4768089fe8bcbac585b8920
nmd974 <jha.payet@gmail.com>
2020-10-26 21:17:38 +0400

    parallax chiffres section + qui sommes nous

--- js/chiffre_anime.js
+++ js/chiffre_anime.js
@@ -1,9 +1,13 @@
+var compteur = 1;
+
 $( document ).ready(function() { // Tells the function to wait to preform until everything on the page has loaded.
     $(window).scroll(function() { // Says this function is preformed continuisly while scrolling.
+        
         var Scroll = $(window).scrollTop() + 1, // This variable finds the distance you have scrolled from the top.
                     SectionOneOffset = $('#chiffres').offset().top; // This variable finds the distance between #section-two and the top. Replace #section-two with the ID of your section. You can duplicate this for as many sections as you want.
+        if (Scroll >= SectionOneOffset && compteur > 0) { 
+            compteur = 0;    
 
-        if (Scroll >= SectionOneOffset) { 
             var n_trajets = 80000;
             var cpt_trajets = 0;
             var trajets =  document.getElementById("trajets");
@@ -13,6 +17,7 @@ $( document ).ready(function() { // Tells the function to wait to preform until 
             var cpt_vh = 0;
             var vh =  document.getElementById("vehicules");
             var delta_vh = Math.ceil((duree * 1000) / n_vh);
+
 
             var n_employee = 33;
             var cpt_employee = 0;
@@ -31,15 +36,15 @@ $( document ).ready(function() { // Tells the function to wait to preform until 
                   if( cpt_trajets < n_trajets ) {
                      setTimeout(countdown, delta_trajets);
                   }
-                vh.innerHTML = `${++cpt_vh}`;
+                vh.innerHTML = `${++cpt_vh + 150}`;
                   if( vh < n_vh ) {
                      setTimeout(countdownVH, delta_vh);
                   }
-                employee.innerHTML = `${++cpt_employee}`;
+                employee.innerHTML = `${++cpt_employee - 47}`;
                   if( employee < n_employee ) {
                      setTimeout(countdownEmployee, delta_employee);
                   }
-                retrait.innerHTML = `${++cpt_retrait}`;
+                retrait.innerHTML = `${++cpt_retrait - 74}`;
                   if( retrait < n_retrait ) {
                      setTimeout(countdownRetrait, delta_retrait);
                   }
@@ -64,6 +69,19 @@ $( document ).ready(function() { // Tells the function to wait to preform until 
                   }
             }
             setTimeout(countdown, delta_trajets);
+            if(cpt_trajets === n_trajets){
+                vh.innerHTML = `${n_vh}`;
+                employee.innerHTML = `${n_employee}`;
+                vh.innerHTML = `${n_retrait}`;
+            }
+        } 
+    });
+    $(window).scroll(function() { // Says this function is preformed continuisly while scrolling.
+        var Scroll = $(window).scrollTop() + 1, // This variable finds the distance you have scrolled from the top.
+                    SectionProduits = $('#produits').offset().top; // This variable finds the distance between #section-two and the top. Replace #section-two with the ID of your section. You can duplicate this for as many sections as you want.
+
+        if (Scroll >= SectionProduits) { 
+
         } 
     });
 });
