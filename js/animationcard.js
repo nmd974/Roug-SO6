da1c3eeed300a083cb47229af0ac14d5445af31f
SAMINADIN Frédéric <saminad974@gmail.com>
2020-10-27 10:57:25 +0400

    js scroll produit

--- js/animationcard.js
+++ js/animationcard.js
@@ -11,3 +11,15 @@ $( document ).ready(function() { // Tells the function to wait to preform until 
         } 
     });
 });
+
+$( document ).ready(function() { // Tells the function to wait to preform until everything on the page has loaded.
+   
+    $(window).scroll(function() { // Says this function is preformed continuisly while scrolling.
+        var Scroll = $(window).scrollTop() + 1, // This variable finds the distance you have scrolled from the top.
+                    SectionProduits = $('#produits').offset().top; // This variable finds the distance between #section-two and the top. Replace #section-two with the ID of your section. You can duplicate this for as many sections as you want.
+
+        if (Scroll >= SectionProduits) { 
+            AOS.init();  
+        } 
+    });
+});
