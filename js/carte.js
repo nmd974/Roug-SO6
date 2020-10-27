1955091959150c12f727932bc4161bd1e819ccf3
SAMINADIN Frédéric <saminad974@gmail.com>
2020-10-27 08:46:51 +0400

    zoom carte

--- js/carte.js
+++ js/carte.js
@@ -14,13 +14,13 @@ var lat = -21.147390;
             function initMap() {
                
                 // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
-                macarte = L.map('map').setView([lat, lon], 11);
+                macarte = L.map('map').setView([lat, lon], 9);
                 // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
                 L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
                     // Il est toujours bien de laisser le lien vers la source des données
                     attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
                     minZoom: 1,
-                    maxZoom: 9
+                    maxZoom: 16
                 }).addTo(macarte);
                  //création du marqueur
                  for (ville in villes) {
